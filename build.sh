e554329c4b93ee373119f69aa9340ea3a4c9e608
hujin <hj_resume@126.com>
2018-02-16 11:57:18 +0800

    ubuntu

--- build.sh
+++ build.sh
@@ -13,18 +13,10 @@ mkdir -p output
 mkdir -p bin
 mkdir -p output/thirdlibs
 
-platform=$(uname -s)
-macos=Darwin
-
-if [ "$platform" == "$macos" ]; then
-    cp -rf thirdlibs/macos/* output/thirdlibs
-else
-    cp -rf thirdlibs/linux/* output/thirdlibs
-fi
-
 cp -rf bin output
 cp -rf tools/supervise.forlove output/bin
 mkdir -p output/conf
 cp -rf conf output
 mkdir -p output/log
 cp tools/control.sh output
+
